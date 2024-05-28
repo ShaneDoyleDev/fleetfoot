397b0a04decc14184c8f61fcecc144b8211e61a5
Shane Doyle <shanedoyle1989@gmail.com>
2024-05-28 15:20:17 +0100

    feat(home/views.py): add login authentication to home view

--- home/views.py
+++ home/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import LoginForm
 
 
 def home(request):
-    """A view for rendering the homepage."""
+    """A view for rendering the homepage and authenticating the user."""
+    # Handle User Login
+    if request.method == 'POST':
+        print("error logging in")
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(
+                    request, f"Welcome {username}. You have been logged in.")
+                return redirect('home')
+            else:
+                messages.error(
+                    request, "There was an error logging in. Please try again!")
+                return redirect('home')
+    else:
+        form = LoginForm()
+        return render(request, 'home/index.html', {'form': form})
     return render(request, 'home/index.html')
 
 
