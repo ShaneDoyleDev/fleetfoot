f1372c440c57e42e83a59d542b9936c306421927
Shane Doyle <shanedoyle1989@gmail.com>
2024-05-28 13:14:02 +0100

    feat(home/forms.py): create LoginForm class

---
+++ home/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    """
+    A form for user login.
+
+    This form is used to authenticate users by validating their username and password.
+    """
+    username = forms.CharField(max_length=100, required=True)
+    password = forms.CharField(widget=forms.PasswordInput, required=True)
