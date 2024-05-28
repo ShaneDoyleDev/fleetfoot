988598f97c04296393521512f188af6ddb232238
Shane Doyle <shanedoyle1989@gmail.com>
2024-05-28 12:17:56 +0100

    feat(home/views.py): setup authentication views

--- home/views.py
+++ home/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def home(request):
-    """A view for rendering the homepage"""
-
+    """A view for rendering the homepage."""
     return render(request, 'home/index.html')
+
+
+def register(request):
+    """View for user registration."""
+    pass
+
+
+def user_login(request):
+    """View for user login."""
+    pass
+
+
+def user_logout(request):
+    """View for user logout."""
+    pass
