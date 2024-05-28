50da0f068df47b3815cc90aae394a1075518961a
Shane Doyle <shanedoyle1989@gmail.com>
2024-05-28 13:12:10 +0100

    feat(home/urls.py): setup authentication urls

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.user_logout, name='logout'),
 ]
