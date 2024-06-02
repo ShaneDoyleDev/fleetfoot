c19d6f1d18acc79b8f13c1cfecdfc67d446af1c9
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-02 13:27:43 +0100

    config(settings.py): replace dot-env package with python-decouple

--- fleetfoot/settings.py
+++ fleetfoot/settings.py
@@ -12,25 +12,24 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-from dotenv import load_dotenv
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Load environment variables from the .env file
-load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 if not SECRET_KEY:
-    raise ValueError('The DJANGO_SECRET_KEY environment variable is not set.')
+    raise ValueError('The SECRET_KEY environment variable is not set.')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
