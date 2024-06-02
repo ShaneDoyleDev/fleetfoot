eb72bf4653e9b2c2d7dfb95b51cad25966a6d614
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-02 13:36:04 +0100

    feat(products/models.py): create Department model class

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.utils import timezone
+from decimal import Decimal
+from django.core.validators import MinValueValidator
+from django.core.exceptions import ValidationError
+from products.utils import set_upload_path
+
+
+class Department(models.Model):
+    """
+    Model representing a department for the store.
+    """
+    name = models.CharField(max_length=20, unique=True)
+    friendly_url_name = models.SlugField(unique=True)
