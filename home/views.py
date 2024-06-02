3c01b2b34b8d43f862f0d1d376eb58cd65809180
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-02 15:38:12 +0100

    refactor(home/views.py): organise imports

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import RegistrationForm, LoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+
+from home.forms import RegistrationForm, LoginForm
+from products.models import Product
 
 
 def home(request):
