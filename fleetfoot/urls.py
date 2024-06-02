b7c5141f61122bf19b61aba296c5f60b9a117403
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-02 16:02:11 +0100

    feat(fleetfoot/urls.py): add products app urls

--- fleetfoot/urls.py
+++ fleetfoot/urls.py
@@ -6,5 +6,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("home.urls")),
+    path('products/', include("products.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
