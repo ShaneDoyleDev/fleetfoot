39a070014d8b4694b86292f4420cef710ffc6154
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-06 11:44:00 +0100

    feat(cart/urls.py): add url for the update cart view

--- cart/urls.py
+++ cart/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.view_cart, name='view-cart'),
     path('add-product/<product_id>',
          views.add_to_cart, name='add-to-cart'),
+    path('update-cart/<product_id>',
+         views.update_cart, name='update-cart'),
 ]
