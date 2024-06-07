373ee4388105f2f99aa2bb1edb41535fbfc5e3ce
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-07 13:17:37 +0100

    feat(checkout/forms.py): create OrderForm for checkout

---
+++ checkout/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from checkout.models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',
+                  'county',)
