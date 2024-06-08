395d5ed8a2d8fb3d346fce8a1840a2c98263eedb
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-08 17:56:05 +0100

    feat(checkout/urls.py): add url for stripe webhook handler

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from checkout.webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout-success/<order_number>',
          views.checkout_success, name='checkout-success'),
+    path('webhook/',
+         webhook, name='webhook'),
 ]
