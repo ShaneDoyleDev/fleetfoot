7ae420b37fa3f9e7798d4132e382e737d28850b1
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-08 18:48:50 +0100

    feat(checkout/urls.py): add url for cache_checkout_data view

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout-success/<order_number>',
          views.checkout_success, name='checkout-success'),
+    path('cache-checkout-data/',
+         views.cache_checkout_data, name='cache-checkout-data'),
     path('webhook/',
          webhook, name='webhook'),
 ]
