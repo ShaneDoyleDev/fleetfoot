ca38b18b9a1c51ae5a4efdc5dc2d0da0ed1e2253
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-08 13:21:23 +0100

    feat(checkout/views.py): save order and line items after purchase

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 
 from cart.context_processors import cart as cart_contents
 from checkout.forms import OrderForm
+from checkout.models import Order, OrderLineItem
 from home.forms import RegistrationForm, LoginForm
+from products.models import Product
 from utils import handle_login, handle_registration
 
 import stripe
@@ -14,18 +18,49 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    cart = request.session.get('cart', {})
+    if request.method == 'POST':
+        cart = request.session.get('cart', {})
 
-    # Setup and create the stripe payment intent
-    current_cart = cart_contents(request)
-    total = current_cart['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
+        order_form = OrderForm(request.POST)
 
-    payment_intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY
-    )
+        if order_form.is_valid():
+            order = order_form.save()
+            for item in cart:
+                try:
+                    product = Product.objects.get(id=item.id)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        product=product,
+                        quantity=item.quantity,
+                    )
+                    order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your cart wasn't found in our database.")
+                    )
+                    order.delete()
+                    return redirect(reverse('view-cart'))
+
+            return redirect(reverse('checkout-success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+    else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(
+                request, "There's nothing in your cart at the moment.")
+            return redirect(reverse('home'))
+
+        # Setup and create the stripe payment intent
+        current_cart = cart_contents(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        payment_intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY
+        )
 
     order_form = OrderForm()
 
