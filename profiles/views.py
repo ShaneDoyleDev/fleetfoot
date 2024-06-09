5ce3f82c77a650c29315a0ccee2656dc0e27c08c
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-09 10:36:28 +0100

    feat(profiles/views.py): create view for the profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def profile(request):
+    """Display the users profile information."""
+    return render(request, 'profiles/profile.html', {})
