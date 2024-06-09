5202a54094dd227cf6f06d0189fab544ced11ad6
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-09 10:15:57 +0100

    feat(profiles/models.py): create Profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,45 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 
-# Create your models here.
+from django_countries.fields import CountryField
+
+from products.models import Product
+
+
+class Profile(models.Model):
+    """
+    Represents a user profile.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(
+        max_length=20, null=True, blank=True)
+    default_street_address1 = models.CharField(
+        max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(
+        max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(
+        max_length=40, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(
+        blank_label='Country', null=True, blank=True)
+
+    def __str__(self):
+        """
+        Return a string representation of the Profile model.
+        """
+        return f'{self.user.username}\'s profile'
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile.
+    """
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
