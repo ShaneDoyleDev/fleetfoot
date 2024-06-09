7865df6dcb8f4592113820d736b0baed13e70b6f
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-09 10:24:03 +0100

    refactor(checkout/models.py): add profile field to Order model

--- checkout/models.py
+++ checkout/models.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.db import models
 from django.db.models import Sum
 from products.models import Product
+from profiles.models import Profile
 
 from django_countries.fields import CountryField
 
@@ -13,6 +14,8 @@ class Order(models.Model):
     Represents an order placed by a customer.
     """
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(
+        Profile, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
