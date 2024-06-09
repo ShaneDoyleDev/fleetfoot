8c8baa16dfa3274992762df4fc1a4618cf222531
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-09 12:49:49 +0100

    refactor(profiles/views.py): add ProfileForm and orders to profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from profiles.models import Profile
+from profiles.forms import ProfileForm
 
 
 def profile(request):
     """Display the users profile information."""
     profile = get_object_or_404(Profile, user=request.user)
+    orders = profile.orders.all()
+    profile_form = ProfileForm(instance=profile)
 
     return render(request, 'profiles/profile.html', {
-        'profile': profile
+        'profile': profile,
+        'orders': orders,
+        'profile_form': profile_form
     })
