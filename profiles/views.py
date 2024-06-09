d04265e2f2a6d67f31aaaa3c14edc9e8e0a62d83
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-09 13:30:08 +0100

    refactor(profiles/views.py): update user profile on post request

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from profiles.models import Profile
 from profiles.forms import ProfileForm
 
@@ -6,6 +7,14 @@ from profiles.forms import ProfileForm
 def profile(request):
     """Display the users profile information."""
     profile = get_object_or_404(Profile, user=request.user)
+
+    if request.method == 'POST':
+        print('called')
+        profile_form = ProfileForm(request.POST, instance=profile)
+        if profile_form.is_valid():
+            profile_form.save()
+            messages.success(
+                request, "You have successfully updated your profile.")
     orders = profile.orders.all()
     profile_form = ProfileForm(instance=profile)
 
