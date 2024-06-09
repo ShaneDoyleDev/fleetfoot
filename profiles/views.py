9b7ddd2e147b6fd6766d374a51a32f866ac7a7eb
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-09 11:11:40 +0100

    feat(profiles/views.py): get profile from logged in user

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from profiles.models import Profile
 
 
 def profile(request):
     """Display the users profile information."""
-    return render(request, 'profiles/profile.html', {})
+    profile = get_object_or_404(Profile, user=request.user)
+
+    return render(request, 'profiles/profile.html', {
+        'profile': profile
+    })
