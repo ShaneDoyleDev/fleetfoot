46e05dc5b273e7013119172f0c5d4a9a7953f29e
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-09 14:48:34 +0100

    feat(checkout/views.py): save profile and attach order on checkout

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,8 @@ import json
 from cart.context_processors import cart as cart_contents
 from checkout.forms import OrderForm
 from checkout.models import Order, OrderLineItem
+from profiles.models import Profile
+from profiles.forms import ProfileForm
 from home.forms import RegistrationForm, LoginForm
 from products.models import Product
 from utils import handle_login, handle_registration
@@ -125,6 +127,26 @@ def checkout_success(request, order_number):
     Handle successful checkouts and show the order details.
     """
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        profile_data = {
+            'default_phone_number': order.phone_number,
+            'default_country': order.country,
+            'default_postcode': order.postcode,
+            'default_town_or_city': order.town_or_city,
+            'default_street_address1': order.street_address1,
+            'default_street_address2': order.street_address2,
+            'default_county': order.county,
+        }
+        user_profile_form = ProfileForm(profile_data, instance=profile)
+        if user_profile_form.is_valid():
+            user_profile_form.save()
 
     message = mark_safe(f"Order successfully processed! </br> A confirmation email will be sent to <strong>{
                         order.email}</strong>.")
