b651cbefd7ebb326e44b254d2fd74360c6145806
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-10 18:53:51 +0100

    refactor(products/urls.py): add url for product_admin view

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('department/<department>', views.products_list, name='products-list'),
     path('product-detail/<int:product_id>',
          views.product_detail, name='product-detail'),
-    path('add-product/', views.add_product, name='add-product'),
-    path('update-stock/', views.update_stock, name='update-stock'),
+    path('product-admin/', views.product_admin, name='product-admin'),
 ]
