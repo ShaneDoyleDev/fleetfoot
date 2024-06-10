7483a7f48bc47a45c63ffa83a31c095d29548d6d
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-10 22:26:06 +0100

    feat(products/urls.py): add url for product_delete view

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('product-admin/', views.product_admin, name='product-admin'),
     path('product-update/<int:product_id>/',
          views.product_update, name='product-update'),
+    path('product-delete/<int:product_id>/',
+         views.product_delete, name='product-delete'),
 ]
