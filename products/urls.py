6c75c7161996ab358c2d9dc12bb6e0871e4285ef
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-10 21:05:48 +0100

    feat(products/urls.py): add url for update_product view

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('department/<department>', views.products_list, name='products-list'),
-    path('product-detail/<int:product_id>',
+    path('product-detail/<int:product_id>/',
          views.product_detail, name='product-detail'),
     path('product-admin/', views.product_admin, name='product-admin'),
+    path('product-update/<int:product_id>/',
+         views.product_update, name='product-update'),
 ]
