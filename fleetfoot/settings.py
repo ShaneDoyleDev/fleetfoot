48420a6a19c0f77876138bc3791dfdc7ed80bb0c
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-11 17:24:57 +0100

    feat(home/context_processors.py): create wishlist id context processor

--- fleetfoot/settings.py
+++ fleetfoot/settings.py
@@ -84,6 +84,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'cart.context_processors.cart',
+                'home.context_processors.wishlist_product_ids',
             ],
         },
     },
