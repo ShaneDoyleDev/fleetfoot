76549043fe883610ffa73b44e6b3bf3c7cf58477
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-11 17:56:42 +0100

    config(settings.py): configure postgres production database settings

--- fleetfoot/settings.py
+++ fleetfoot/settings.py
@@ -99,12 +99,18 @@ WSGI_APPLICATION = 'fleetfoot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+if ENVIRONMENT == 'development':
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(config('DATABASE_URL'))
+    }
 
 
 # Password validation
