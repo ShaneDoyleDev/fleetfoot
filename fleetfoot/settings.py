78e8925299cfb28aa03ea84a2a146e96fcac33b0
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-11 17:54:50 +0100

    feat(settings.py): import dj_database_url in settings.py file

--- fleetfoot/settings.py
+++ fleetfoot/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
