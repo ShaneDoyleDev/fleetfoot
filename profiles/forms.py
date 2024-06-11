638ccc22033efcfe81322c50d576ad0815dca9a8
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-11 09:43:15 +0100

    feat(profiles/forms.py): create ReviewForm

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,8 +1,20 @@
 from django import forms
-from .models import Profile
+from .models import Profile, Review
 
 
 class ProfileForm(forms.ModelForm):
+    """
+    A form for creating or updating user profiles.
+    """
     class Meta:
         model = Profile
         exclude = ('user',)
+
+
+class ReviewForm(forms.ModelForm):
+    """
+    A form for creating or updating a review.
+    """
+    class Meta:
+        model = Review
+        fields = ['title', 'content']
