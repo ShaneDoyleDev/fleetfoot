9f2a967de8f2c432e197a0aa3b368879bb34bc3b
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-11 14:33:57 +0100

    feat(templatetags/range_filter.py): create times custom filter tag

---
+++ products/templatetags/range_filter.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def times(number):
+    """
+    Returns a range object that represents a sequence of numbers from 0 up to (but not including) the given number.
+    """
+    return range(number)
