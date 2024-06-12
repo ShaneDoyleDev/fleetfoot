167511ccfcad66f98e6aca04680f998a1ecc2b8c
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-12 17:55:47 +0100

    config(settings.py): add django_extensions to installed apps

--- fleetfoot/settings.py
+++ fleetfoot/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'tailwind',
     'theme',
     'django_browser_reload',
