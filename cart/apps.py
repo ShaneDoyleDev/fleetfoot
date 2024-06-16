1e3c49c8af97e6b1ec2d9760230318631d8999bc
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-16 15:00:10 +0100

    doc(cart/apps.py): add docstring to CartConfig class

--- cart/apps.py
+++ cart/apps.py
@@ -2,5 +2,12 @@ from django.apps import AppConfig
 
 
 class CartConfig(AppConfig):
+    """
+    AppConfig for the 'cart' app.
+
+    This class represents the configuration for the 'cart' app
+    in the Django project.
+    It sets the default auto field and the name of the app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cart'
