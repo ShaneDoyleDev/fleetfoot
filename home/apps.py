887b45626ef386a2b6b3e8c1db661ae95259e1fd
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-16 15:59:38 +0100

    doc(home/apps.py): add docstring to HomeConfig class

--- home/apps.py
+++ home/apps.py
@@ -2,5 +2,11 @@ from django.apps import AppConfig
 
 
 class HomeConfig(AppConfig):
+    """
+    AppConfig for the 'home' app.
+    This class represents the configuration for the
+    'home' app in the Django project.
+    It sets the default auto field and specifies the name of the app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
