218333681693f385731d1b96a290d63fdf793ed3
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-16 15:58:29 +0100

    docs(home/context_processors.py): add docstring to wishlist_product_ids

--- home/context_processors.py
+++ home/context_processors.py
@@ -1,4 +1,7 @@
 def wishlist_product_ids(request):
+    """
+    Retrieves the product IDs from the user's wishlist.
+    """
     if request.user.is_authenticated:
         ids = list(request.user.profile.wishlists.values_list(
             'product_id', flat=True))
