38e11676cbda9b19c4b3c8f989dc74cd4573dfd4
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-16 14:38:17 +0100

    format(products/range_filter.py): shorten length of docstring

--- products/templatetags/range_filter.py
+++ products/templatetags/range_filter.py
@@ -6,6 +6,7 @@ register = template.Library()
 @register.filter
 def times(number):
     """
-    Returns a range object that represents a sequence of numbers from 0 up to (but not including) the given number.
+    Returns a range object that represents a sequence
+    of numbers from 0 up to (but not including) the given number.
     """
     return range(number)
