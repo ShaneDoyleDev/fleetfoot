c6d225ad2c3ac72a8a6dfd54bc9986a1f0c539a7
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-17 11:19:22 +0100

    feat(modals.js): add modals.js script

---
+++ static/js/modals.js
@@ -0,0 +1,44 @@
+// DOM Selectors
+const registrationForm = document.querySelector(".registration-form");
+const openRegistrationBtns = document.querySelectorAll(".open-registration");
+const closeRegistrationBtn = document.querySelector(".close-registration");
+
+const loginForm = document.querySelector(".login-form");
+const openLoginBtns = document.querySelectorAll(".open-login");
+const closeLoginBtn = document.querySelector(".close-login");
+
+/**
+ * Shows a form by removing the "hidden" class and adding the "flex" class.
+ */
+function showForm(form) {
+  form.classList.remove("hidden");
+  form.classList.add("flex");
+}
+
+/**
+ * Hides the specified form by adding the "hidden" class and removing the "flex" class.
+ */
+function hideForm(form) {
+  form.classList.add("hidden");
+  form.classList.remove("flex");
+}
+
+// Event listeners
+openRegistrationBtns.forEach((btn) => {
+  btn.addEventListener("click", () => showForm(registrationForm));
+});
+closeRegistrationBtn.addEventListener("click", () =>
+  hideForm(registrationForm)
+);
+
+openLoginBtns.forEach((btn) => {
+  btn.addEventListener("click", () => showForm(loginForm));
+});
+closeLoginBtn.addEventListener("click", () => hideForm(loginForm));
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    hideForm(registrationForm);
+    hideForm(loginForm);
+  }
+});
