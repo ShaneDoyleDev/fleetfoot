7605bfecb60d404aac23d4ee9c00be7a7eb377b9
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-18 23:08:20 +0100

    feat(products/admin.py): add productStock to admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from products.models import Product
+from products.models import Product, ProductStock
 
 
 admin.site.register(Product)
+admin.site.register(ProductStock)
