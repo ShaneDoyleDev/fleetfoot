91b2edbec06c101ef46e5883b377b38ed8b42153
Shane Doyle <shanedoyle1989@gmail.com>
2024-06-18 23:13:30 +0100

    feat(products/urls.py): add url for get_product_sizes view

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('department/<department>', views.products_list, name='products-list'),
+    path('get-product-sizes/',
+         views.get_product_sizes, name='get-product-sizes'),
     path('product-detail/<int:product_id>/',
          views.product_detail, name='product-detail'),
     path('product-admin/', views.product_admin, name='product-admin'),
