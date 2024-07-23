00ce2da0701e8bedf82f5b948baa92a1524ca90f
Shane Doyle <shanedoyle1989@gmail.com>
2024-07-23 23:03:29 +0100

    fix(static/deleteConfirmation.js): select corresponding delete modals

--- static/js/deleteConfirmation.js
+++ static/js/deleteConfirmation.js
@@ -9,6 +9,11 @@ const hideConfirmationBtns = document.querySelectorAll(
 /**
  * Shows a delete confirmation modal for a product by removing the "hidden" class and adding the "flex" class.
  */
+function showDeleteConfirmation(event) {
+  const productId = event.currentTarget.dataset.productId;
+  const deleteConfirmation = document.querySelector(
+    `.delete-confirmation-modal[data-product-id="${productId}"]`
+  );
   deleteConfirmation.classList.remove("hidden");
   deleteConfirmation.classList.add("flex");
 }
@@ -16,6 +21,10 @@ const hideConfirmationBtns = document.querySelectorAll(
 /**
  * Hides a delete confirmation modal for a product by removing the "hidden" class and adding the "flex" class.
  */
+function hideDeleteConfirmation(event) {
+  const deleteConfirmation = event.currentTarget.closest(
+    ".delete-confirmation-modal"
+  );
   deleteConfirmation.classList.add("hidden");
   deleteConfirmation.classList.remove("flex");
 }
